c066f7a9771219583930b3c69db63ac80cb97608
Andrei <dandrei.web@gmail.com>
2022-09-22 19:29:23 +0200

    get crypto list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const client = axios.create({
+  baseURL: "https://api.coinstats.app/public/v1/coins?skip=0&limit=100¤cy=INR",
+});
+
 function App() {
+  const [crypto, setCrypto] = useState([]);
+
+  useEffect(() => {
+    async function getCrypto() {
+      const resp = await client.get();
+      setCrypto(resp.data.coins);
+    }
+    getCrypto();
+  }, []);
+
   return (
     <div>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, molestias.</h1>
+      <pre>
+        {crypto.map((item, key) => (
+          <div key={key}>
+            <h1>{item.name}</h1>
+          </div>
+        ))}
+      </pre>
     </div>
   );
 }
